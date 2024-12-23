5c63783a1d573be51177576f0d4e5459e2781ae3
kurmamahendra417 <mahendrakurmamahendra@gmail.com>
2024-12-23 12:29:41 +0530

    0to10and10to0.c

---
+++ 0to10and10to0.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main() 
+{
+ int i,n,j;   
+    printf("enter n");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        printf("%d",i);
+         }
+         for(j=n;j>=0;j--)
+         {
+             printf("%d",j);
+         }
+}
+    
