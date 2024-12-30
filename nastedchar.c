2a81ef02d1bc6932a7846d35d608efc9dbf35b65
kurmamahendra417 <mahendrakurmamahendra@gmail.com>
2024-12-30 12:05:11 +0530

    nastedchar.c

---
+++ nastedchar.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+void main() 
+{
+    int i,j;
+    char n='A';
+    for(i=0;i<5;++i)
+    {
+        for(j=0;j<5;++j)
+    {
+    printf("%d/",n++);
+    
+    }
+}
+}
