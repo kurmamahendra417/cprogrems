33746141e024e42c2dc47c16cc32b214d417a475
kurmamahendra417 <mahendrakurmamahendra@gmail.com>
2024-12-30 11:32:22 +0530

    nastednoofi.c

---
+++ nastednoofi.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() 
+{
+    int i,j,n=1;
+    for(i=0;i<5;++i)
+    {
+        for(j=0;j<5;++j)
+    {
+    printf("%d",i);
+{
+    printf("\n");
+}
+    }
+}
+}
