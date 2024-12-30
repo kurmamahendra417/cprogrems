88bffd74663f4c3f4637d6955bbe37baa2db9fca
kurmamahendra417 <mahendrakurmamahendra@gmail.com>
2024-12-30 11:22:45 +0530

    nastedstar.c

---
+++ nastedstar.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() 
+{
+    int i,j;
+    for(i=0;i<5;++i)
+    {
+        for(j=0;j<5;++j)
+    {
+    printf("*");
+
+    }
+}
+}
